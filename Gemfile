affaee8b43b38e113cf095eec91621c944911079
Nico Rueda <nicoru3da@gmail.com>
2013-05-04 12:26:13 -0700

    initial commit

--- Gemfile
+++ Gemfile
@@ -1,4 +1,6 @@
 source 'https://rubygems.org'
-
+gem "facebook-login", "~> 0.3.0"
 gem 'sinatra'
 gem "sinatra-reloader", "~> 1.0"
+gem 'koala'
+gem 'omniauth'
--- myapp.rb
+++ myapp.rb
@@ -1,6 +1,6 @@
 require 'sinatra'
 require 'sinatra/reloader'
-require 'json'
+require 'koala'
 
 enable :sessions
 
@@ -27,7 +27,13 @@ end
 
 get '/secrets' do
   return redirect '/' unless session[:user] == 'nico'
-  "Waffle"
+  @graph = Koala::Facebook::API.new('BAACEdEose0cBAG6WdTbI4GK9vCOJ3kOEezBcROk9YyXjoxWQjRjZA5fEQZC9yh8cMLHSjixqW5ZAEoGZAwhQosfuX88GcMm4ZBDJV3QA7UwM8IVvY2I8NgLGnfc4kLS9kG3cTikmWiHni3bfCGT5bby3NK9Qke4bmDYlschSmkOmfOrS5ynNC9JQEVY0KozCN2qdnn18Aftf9BjcgJMXrKphGlZBUHqAcF8XJN3MgkuQZDZD')
+  @picture = @graph.get_picture('nicolas.rueda.7737')
+  erb :secrets
+end
+
+post '/go_here' do
+  redirect "http://#{params[:place]}.com"
 end
 
 get '/logout' do
